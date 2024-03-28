046e60b9bc5bb61336b3ae5a0f09a7a0ed08ba72
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-03-29 01:33:05 +0530

    Time: 67 ms (68.48%), Space: 60.6 MB (44.28%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.java
@@ -1,15 +1,15 @@
 class Solution {
-public:
-    int maxSubarrayLength(vector<int>& nums, int k) {
-        int n = nums.size(), ans = 0;
-        unordered_map<int, int>mp;
-        for(int high = 0, low = 0; high < n; high++) {
-            mp[nums[high]]++;
-            while(mp[nums[high]] > k) {
-                mp[nums[low++]]--;
+    public int maxSubarrayLength(int[] nums, int k) {
+        int n = nums.length, ans = 0;
+        Map<Integer, Integer>mp = new HashMap<>();
+        for(int high = 0, low = 0; high<n; high++) {
+            mp.put(nums[high], mp.getOrDefault(nums[high], 0) + 1);
+            while(mp.get(nums[high]) > k) {
+                mp.put(nums[low], mp.getOrDefault(nums[low], 0) - 1);
+                low++;
             }
-            ans = max(ans, high - low + 1);
+            ans = Math.max(ans, high - low + 1);
         }
         return ans;
     }
-};
+}
