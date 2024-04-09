f5775b88bff2a9a0b742c036bb727c0ef88207d9
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-09 14:18:01 +0530

    Time: 63 ms (35.92%), Space: 17.8 MB (82.09%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>&stones, int cur, vector<int>&visited) {
-        if(visited[cur]) return 0;
-        visited[cur] = 1;
-        int ans = 0;
-        for(int i=0; i<stones.size(); i++) {
-            if(!visited[i] && (stones[i][0] == stones[cur][0] || stones[i][1] == stones[cur][1])) {
-                ans += dfs(stones, i, visited) + 1;
-            }
+    int find(int u, vector<int>&parent) {
+        if(parent[u] == -1) return u;
+        return parent[u] = find(parent[u], parent);
+    }
+    bool _union(int u, int v, vector<int>&parent, vector<int>&rank) {
+        int pu = find(u, parent);
+        int pv = find(v, parent);
+        if(pu == pv) return false;
+        if(rank[pu] > rank[pv]) {
+            parent[pv] = pu;
+            rank[pu] += 1;
         }
-        return ans;
+        else {
+            parent[pu] = pv;
+            rank[pv] += 1;
+        }
+        return true;
     }
     int removeStones(vector<vector<int>>& stones) {
-        int n = stones.size(), ans = 0;
-        vector<int>visited(n, 0);
+        int n = stones.size();
+        int cnt = 0;
+        vector<int>parent(n+1, -1), rank(n+1, 0);
         for(int i=0; i<n; i++) {
-            if(visited[i]) continue;
-            ans += dfs(stones, i, visited);
+            for(int j=i+1; j<n; j++) {
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    if(_union(i, j, parent, rank)) cnt++;
+                }
+            }
         }
-        return ans;
+        return cnt;
     }
 };
