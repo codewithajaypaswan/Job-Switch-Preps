318a20b1d498fdd508f90a5c271bb407e8c5db70
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-16 09:08:21 +0530

    Time: 5 ms (48.36%), Space: 8.1 MB (60.45%) - LeetHub

---
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        if(preorder.empty()) return true;
+        preorder += ',';
+        int cnt = 1;
+        for(int i=0; i<preorder.size(); i++) {
+            if(preorder[i] != ',') continue;
+            cnt--;
+            if(cnt < 0) return false;
+            if(preorder[i-1] != '#') cnt += 2;
+        }
+        return cnt == 0;
+    }
+};
