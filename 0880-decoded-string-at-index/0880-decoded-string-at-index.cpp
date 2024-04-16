8ddad1241915acac4a766e0495c68f58a44fd52a
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-16 12:02:25 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (50.10%) - LeetHub

---
+++ 0880-decoded-string-at-index/0880-decoded-string-at-index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string decodeAtIndex(string s, int k) {
+        long long len = 0;
+        for(char c:s) {
+            if(c >= '2' && c <= '9') len *= (c - '0');
+            else len++;
+        }
+        for(int i=s.size()-1; i>=0; i--) {
+            k = k%len;
+            if(k == 0 && !isdigit(s[i])) {
+                string ans = "";
+                ans += s[i];
+                return ans;
+            }
+            else if(s[i] >= '2' && s[i] <= '9') len /= (s[i] - '0');
+            else len--;
+        }
+        return "";
+    }
+};
