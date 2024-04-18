a007e3309f5f7c4da53667a5f3579813f8031736
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-18 07:52:11 +0530

    Time: 4 ms (99.25%), Space: 20.4 MB (32.78%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -14,13 +14,14 @@ public:
     unordered_map<long, int>mp;
     int ans = 0;
     int pathSum(TreeNode* root, int targetSum) {
-        mp[0] = 1;
+        if(root && !root->left && !root->right) return root->val == targetSum;
         dfs(root, targetSum, 0);
         return ans;
     }
     void dfs(TreeNode* root, int targetSum, long sum) {
         if(!root) return;
         sum += root->val;
+        if(sum == targetSum) ans++;
         if(mp.find(sum - targetSum) != mp.end()) {
             ans += mp[sum - targetSum];
         }
