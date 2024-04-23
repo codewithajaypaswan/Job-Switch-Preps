4b91007bba1e54766361da397c07b8c005069637
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-24 01:04:23 +0530

    Time: 196 ms (70.34%), Space: 114.9 MB (90.79%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
+    int dfs(vector<int>adj[], int cur, vector<int>&informTime) {
+        int res = 0;
+        for(int child:adj[cur]) {
+            res = max(res, dfs(adj, child, informTime));
+        }
+        return res + informTime[cur];
+    }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         vector<int>adj[n];
         for(int i=0; i<n; i++) {
             if(headID == i) continue;
             adj[manager[i]].push_back(i);
         }
-        queue<pair<int, int>>q;
-        q.push({headID, 0});
-        int ans = 0;
-        while(!q.empty()) {
-            auto[cur, time] = q.front(); q.pop();
-            ans = max(ans, time);
-            for(auto it:adj[cur]) {
-                q.push({it, time + informTime[cur]});
-            }
-        }
-        return ans;
+        return dfs(adj, headID, informTime);
     }
 };
