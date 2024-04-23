b299e6eb2e5bf0cb5c18a5dd3d52b22f02ebb2cb
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-24 01:22:08 +0530

    Time: 32 ms (30.66%), Space: 21.6 MB (23.81%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -2,20 +2,25 @@ class Solution {
 public:
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
-    void dfs(vector<vector<char>>&grid, int x, int y) {
-        if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == '0') return;
-        grid[x][y] = '0';
-        for(int i=0; i<4; i++) {
-            dfs(grid, x+dx[i], y+dy[i]);
-        }
-    }
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size(), m = grid[0].size(), ans = 0;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == '1') {
-                    dfs(grid, i, j);
                     ans++;
+                    queue<pair<int,int>>q;
+                    q.push({i, j});
+                    while(!q.empty()) {
+                        auto[x, y] = q.front(); q.pop();
+                        for(int i=0; i<4; i++) {
+                            int curX = x + dx[i];
+                            int curY = y + dy[i];
+                            if(curX >= 0 && curX < n && curY >= 0 && curY < m && grid[curX][curY] == '1') {
+                                grid[curX][curY] = '0';
+                                q.push({curX, curY});
+                            }
+                        }
+                    }
                 }
             }
         }
