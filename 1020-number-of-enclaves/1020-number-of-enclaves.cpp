dfac75f4085062a045854c6d414d5a4b24cf0dbb
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-24 00:28:09 +0530

    Time: 37 ms (98.65%), Space: 31.8 MB (62.21%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
+    void dfs(vector<vector<int>>&grid, int x, int y) {
+       if(x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() || grid[x][y] == 0) return;
+        grid[x][y] = 0;
+        for(int i=0; i<4; i++) {
+            dfs(grid, x+dx[i], y+dy[i]);
+        }
+    }
     int numEnclaves(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         queue<pair<int,int>>q;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(i == 0 || i == n-1 || j == 0 || j == m-1) {
-                    if(grid[i][j]) {
-                        q.push({i, j});
-                        grid[i][j] = 0;
-                    }
-                }
-            }
-        }
-        while(!q.empty()) {
-            auto[x, y] = q.front(); q.pop();
-            for(int i=0; i<4; i++) {
-                int curX = x + dx[i];
-                int curY = y + dy[i];
-                if(curX >= 0 && curX < n && curY >= 0 && curY < m && grid[curX][curY] == 1) {
-                    grid[curX][curY] = 0;
-                    q.push({curX, curY});
+                    dfs(grid, i, j);
                 }
             }
         }
