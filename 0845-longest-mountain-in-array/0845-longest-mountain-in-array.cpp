7e73c98582a5325efa5c73f546c6a96276dbf8a0
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-25 09:17:41 +0530

    Time: 8 ms (87.46%), Space: 20.9 MB (68.07%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int longestMountain(vector<int>& arr) {
-        int n = arr.size(), ans = 0;
-        vector<int>left(n, 1), right(n, 1);
-        for(int i=1; i<n; i++) {
-            if(arr[i] > arr[i-1]) left[i] = left[i-1] + 1;
-        }
-        for(int i=n-2; i>=0; i--) {
-            if(arr[i] > arr[i+1]) right[i] = right[i+1] + 1;
-        }
-        for(int i=1; i<n-1; i++) {
-            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
-                ans = max(ans, left[i-1] + right[i+1] + 1);
+        int inc = 0, dec = 0, n = arr.size();
+        int i = 1, ans = 0;
+        while(i < n) {
+            inc = 0, dec = 0;
+            while(i < n && arr[i-1] < arr[i]) i++, inc++;
+            while(i < n && arr[i-1] > arr[i]) i++, dec++;
+            if(inc && dec) {
+                ans = max(ans, inc + dec + 1);
             }
+            while(i < n && arr[i-1] == arr[i]) i++;
         }
         return ans;
     }
