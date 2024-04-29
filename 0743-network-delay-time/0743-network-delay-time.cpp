86732beb7df44b4f8f740926f7137f22d5e2933b
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-04-30 00:37:09 +0530

    Time: 995 ms (5.01%), Space: 275.3 MB (5.07%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int, int>>adj[n + 1];
-        for(auto e:times) {
-            adj[e[0]].push_back({e[1], e[2]});
-        }
-        priority_queue<pair<int, int>, vector<pair<int,int>>, greater<pair<int,int>>>pq;
-        vector<int>dist(n+1, 1e9), vis(n+1, 0);
+        vector<int>dist(n+1, 1e9);
         dist[k] = 0;
-        pq.push({0, k});
-        while(!pq.empty()) {
-            auto it = pq.top(); pq.pop();
-            int u = it.second, wt = it.first;
-            vis[u] = 1;
-            for(auto[v, wt]:adj[u]) {
-                if(!vis[v] && dist[v] > dist[u] + wt) {
+        for(int i=0; i<n-1; i++) {
+            for(auto t:times) {
+                int u = t[0], v = t[1], wt = t[2];
+                if(dist[u] != 1e9 && dist[u] + wt < dist[v]) {
                     dist[v] = dist[u] + wt;
-                    pq.push({dist[v], v});
                 }
             }
         }
