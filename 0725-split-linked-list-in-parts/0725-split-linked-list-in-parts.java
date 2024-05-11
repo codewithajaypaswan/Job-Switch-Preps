ae00585fafcfc0899916584e99db1ad3c577db48
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-11 08:11:01 +0530

    Time: 0 ms (100.00%), Space: 43.3 MB (58.99%) - LeetHub

--- 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
+++ 0725-split-linked-list-in-parts/0725-split-linked-list-in-parts.java
@@ -1,33 +1,32 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
+ * public class ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
  */
 class Solution {
-public:
-    vector<ListNode*> splitListToParts(ListNode* head, int k) {
-        vector<ListNode*>ans(k, NULL);
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] ans = new ListNode[k];
         int len = 0;
-        ListNode* cur = head;
-        while(cur) {
+        ListNode cur = head;
+        while(cur != null) {
+            cur = cur.next;
             len++;
-            cur = cur->next;
         }
         int equal = len/k, rem = len%k;
         for(int i=0; i<k; i++, rem--) {
             ans[i] = head;
-            ListNode* prev = NULL;
-            for(int j=0; j<equal + (rem > 0); j++) {
+            ListNode prev = null;
+            for(int j=0; j<equal + ((rem > 0) ? 1 : 0); j++) {
                 prev = head;
-                head = head->next;
+                head = head.next;
             }
-            if(prev) prev->next = NULL;
+            if(prev != null) prev.next = null;
         }
         return ans;
     }
-};
+}
