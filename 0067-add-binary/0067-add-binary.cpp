88716f8322408438db752833b7e8d996a8c8f0aa
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 12:39:41 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (78.01%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -2,28 +2,13 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         int n = a.size(), m = b.size();
-        int i = n-1, j = m-1, carry = 0;
-        string ans = "";
-        while(i >=0 && j >= 0) {
-            int val = (a[i] - '0') + (b[j] - '0') + carry;
-            ans += (val)%2 + '0';
-            carry = val/2;
-            i--; j--;
-        }
-        while(i >= 0) {
-            int val = (a[i] - '0') + carry;
-            ans += (val)%2 + '0';
-            carry = val/2;
-            i--;
-        }
-        while(j >= 0) {
-            int val = (b[j] - '0') + carry;
-            ans += (val)%2 + '0';
-            carry = val/2;
-            j--;
-        }
-        if(carry) {
-            ans += '0' + carry;
+        string ans;
+        int i = n-1, j = m-1, num = 0;
+        while(i >=0 || j>=0 || num == 1) {
+            num += i>=0 ? (a[i--]-'0') : 0;
+            num += j>=0 ? (b[j--]-'0') : 0;
+            ans.push_back((num%2) + '0');
+            num = num/2;
         }
         reverse(ans.begin(), ans.end());
         return ans;
