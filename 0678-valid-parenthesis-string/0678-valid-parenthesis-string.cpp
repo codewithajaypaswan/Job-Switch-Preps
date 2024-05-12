8296729ec0a9e4b44fa99116a3a13ff43219e5eb
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 13:52:50 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (72.34%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        vector<int>left, star;
-        for(int i=0; i<s.size(); i++) {
-            if(s[i] == '(') {
-                left.push_back(i);
-            }
+        vector<int>star, left;
+        int n = s.size();
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(') left.push_back(i);
             else if(s[i] == '*') star.push_back(i);
-            else {
-                if(left.size() > 0) left.pop_back();
-                else if(star.size() > 0) star.pop_back();
-                else return false;
-            }
+            else if(left.size() > 0) left.pop_back();
+            else if(star.size() > 0) star.pop_back();
+            else return false;
         }
-        
         while(left.size() && star.size()) {
             if(left.back() < star.back()) {
                 left.pop_back();
