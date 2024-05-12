bcb93ae0d94c6d1eb7ea7d060a5ed1579b858bc6
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 13:02:11 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (33.38%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    vector<int>kmp(string s) {
-        int n = s.size(), i = 1, j = 0;
-        vector<int>lps(n);
-        lps[0] = 0;
+    vector<int> kmp(string &str) {
+        int n = str.size();
+        vector<int>lps(n, 0);
+        int i = 1, j = 0;
         while(i < n) {
-            if(s[i] == s[j]) {
+            if(str[i] == str[j]) {
                 lps[i++] = ++j;
             }
             else if(j == 0) i++;
@@ -15,19 +15,15 @@ public:
     }
     int strStr(string haystack, string needle) {
         vector<int>lps = kmp(needle);
-        int i = 0, j = 0;
         int n = haystack.size(), m = needle.size();
+        int i = 0, j = 0;
         while(i < n && j < m) {
             if(haystack[i] == needle[j]) {
                 i++; j++;
                 if(j == m) return i - m;
             }
-            else if(j == 0) {
-                i++;
-            }
-            else {
-                j = lps[j-1];
-            }
+            else if(j == 0) i++;
+            else j = lps[j-1];
         }
         return -1;
     }
