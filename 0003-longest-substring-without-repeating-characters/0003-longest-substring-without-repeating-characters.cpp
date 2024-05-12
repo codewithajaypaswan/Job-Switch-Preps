9400ffc52b1debbda978df094d302a2560db9d35
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 13:10:03 +0530

    Time: 10 ms (72.60%), Space: 11.2 MB (61.22%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,10 +4,11 @@ public:
         unordered_map<char, int>mp;
         int ans = 0;
         for(int high = 0, low = 0; high < s.size(); high++) {
-            while(mp[s[high]] > 0) {
-                mp[s[low++]]--;
-            }
             mp[s[high]]++;
+            while(mp[s[high]] > 1) {
+                mp[s[low]]--;
+                low++;
+            }
             ans = max(ans, high - low + 1);
         }
         return ans;
