7669d207df667ebc7b37f5f9d4b8eae1e9d72a9b
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 07:14:39 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (48.89%) - LeetHub

--- 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
+++ 1016-binary-string-with-substrings-representing-1-to-n/1016-binary-string-with-substrings-representing-1-to-n.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     bool queryString(string s, int n) {
         for(int i=1;i<=n;i++){
-            bitset<32> b(i);
-            string t=b.to_string();
-            // cout<<t<<endl;
-            int one=t.find('1');
-            // cout<<one<<endl;
-            string p=t.substr(one);
-            // cout<<p<<endl;
-            if(s.find(p)==-1){
-                return false;
-            }
+            bitset<32>b(i);
+            string str = b.to_string();
+            int one = str.find("1");
+            string digit = str.substr(one);
+            if(s.find(digit) == -1) return false;
         }
         return true;
     }
