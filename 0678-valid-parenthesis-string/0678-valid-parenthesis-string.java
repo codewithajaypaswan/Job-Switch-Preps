f7f87bd51d3a856384b25c0829a9b3e1aeb32b8a
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-12 13:54:33 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (72.34%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        vector<int>star, left;
+        int n = s.size();
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(') left.push_back(i);
+            else if(s[i] == '*') star.push_back(i);
+            else if(left.size() > 0) left.pop_back();
+            else if(star.size() > 0) star.pop_back();
+            else return false;
+        }
+        while(left.size() && star.size()) {
+            if(left.back() < star.back()) {
+                left.pop_back();
+                star.pop_back();
+            }
+            else {
+                return false;
+            }
+        }
+        return left.empty();
+    }
+};
