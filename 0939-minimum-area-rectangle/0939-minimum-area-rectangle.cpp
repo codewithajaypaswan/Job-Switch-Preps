e70763220382b409012ccea0fcb81a4a54838a47
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-13 15:58:49 +0530

    Time: 681 ms (25.67%), Space: 27.1 MB (34.47%) - LeetHub

--- 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
+++ 0939-minimum-area-rectangle/0939-minimum-area-rectangle.cpp
@@ -2,20 +2,20 @@ class Solution {
 public:
     int minAreaRect(vector<vector<int>>& points) {
         unordered_map<int, unordered_set<int>>mp;
+        int n = points.size(), ans = INT_MAX;
         for(auto p:points) {
             mp[p[0]].insert(p[1]);
         }
-        int minArea = INT_MAX, n = points.size();
         for(int i=0; i<n; i++) {
-            for(int j=i+1; j<n; j++) {
+            for(int j=0; j<n; j++) {
                 int x1 = points[i][0], y1 = points[i][1];
                 int x2 = points[j][0], y2 = points[j][1];
                 if(x1 == x2 || y1 == y2) continue;
                 if(mp[x1].find(y2) != mp[x1].end() && mp[x2].find(y1) != mp[x2].end()) {
-                    minArea = min(minArea, abs(x1 - x2) * abs(y1 - y2));
+                    ans = min(ans, abs(x2 - x1) * abs(y2 - y1));
                 }
             }
         }
-        return minArea == INT_MAX ? 0 : minArea;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
