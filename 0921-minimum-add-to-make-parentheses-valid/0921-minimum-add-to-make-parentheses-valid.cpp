0edaad0d14617daae0aa0a7980dcea79cf2b40a8
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-15 12:55:55 +0530

    Time: 3 ms (43.95%), Space: 7.5 MB (69.31%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int minAddToMakeValid(string s) {
         int leftPar = 0, rightPar = 0;
-        for(char c:s) {
-            if(c == '(') leftPar++;
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] == '(') leftPar++;
             else if(leftPar) leftPar--;
             else rightPar++;
         }
