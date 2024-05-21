3690183863a52d3217dd6db0897f8edc0fa7a7da
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-21 07:42:01 +0530

    Time: 61 ms (70.98%), Space: 45.3 MB (63.24%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -9,9 +9,7 @@ public:
             if(!st.empty()) prev_smaller[i] = st.top();
             st.push(i);
         }
-        
         while(!st.empty()) st.pop();
-        
         for(int i=n-1; i>=0; i--) {
             while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
             if(!st.empty()) next_smaller[i] = st.top();
@@ -19,10 +17,10 @@ public:
         }
         long long ans = 0, mod = 1e9+7;
         for(int i=0; i<n; i++) {
-            int l = i - prev_smaller[i];
-            int r = next_smaller[i] - i;
-            ans = (ans + (1LL * arr[i] * l * r)%mod)%mod;
+            int leftSmaller = i - prev_smaller[i];
+            int rightSmaller = next_smaller[i] - i;
+            ans = (ans%mod + (1LL * arr[i] * leftSmaller * rightSmaller)%mod)%mod;
         }
-        return ans%mod;
+        return ans;
     }
 };
