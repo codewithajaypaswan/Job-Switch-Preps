1f9e309bf30a07f59405af80949de19f772c69a9
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-24 03:48:42 +0530

    Time: 787 ms (16.29%), Space: 38.7 MB (33.60%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    unordered_map<int, int>mp;
+    int solve(vector<int>&nums, int k, int ind) {
+        if(ind == nums.size()) return 1;
+        int ans = solve(nums, k, ind+1); // when skip
+        if(mp[nums[ind] - k] == 0 && mp[nums[ind] + k] == 0) {
+            mp[nums[ind]]++; // take this nums[i]
+            ans += solve(nums, k, ind+1);
+            mp[nums[ind]]--; // revert this nums[i]
+        }
+        return ans;
+    }
+    int beautifulSubsets(vector<int>& nums, int k) {
+        return solve(nums, k, 0) - 1;
+    }
+};
