9774931998027bd35164ced0d310b9e7f8713835
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-24 04:09:31 +0530

    Time: 398 ms (45.42%), Space: 35.6 MB (96.33%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     unordered_map<int, int>mp;
-    int solve(vector<int>&nums, int k, int ind) {
-        if(ind == nums.size()) return 1;
-        int ans = solve(nums, k, ind+1); // when skip
-        if(mp[nums[ind] - k] == 0) {
-            mp[nums[ind]]++; // take this nums[i]
-            ans += solve(nums, k, ind+1);
-            mp[nums[ind]]--; // revert this nums[i]
+    int ans = 0;
+    int solve(vector<int>&nums, int k, int ind, int mask) {
+        if(ind == nums.size()){
+            return 1;
         }
+        bool include = true;
+        int ans = 0;
+        for(int i=0; i<ind; i++) {
+            if(mask&(1<<i)) {
+                if(abs(nums[i] - nums[ind]) == k) {
+                    include = false;
+                    break;
+                }
+            }
+        }
+        if(include) ans += solve(nums, k, ind+1, mask|(1<<ind));
+        ans += solve(nums, k, ind+1, mask);
         return ans;
     }
     int beautifulSubsets(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return solve(nums, k, 0) - 1;
+        return solve(nums, k, 0, 0) - 1;
+        return ans;
     }
 };
