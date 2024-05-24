4248561e19d82f71c38021bef990aefe3cb130ff
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-24 16:20:41 +0530

    Time: 3 ms (88.93%), Space: 12.6 MB (82.72%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    int solve(vector<string>&words, vector<int>&freq, vector<int>&score, int cur) {
-        if(cur >= words.size()) return 0;
-        int noTake = solve(words, freq, score, cur+1);
-        int sc = 0, flag = 1, take = 0;
-        for(char c:words[cur]) {
-            if(freq[c - 'a'] == 0) {
-                flag = 0;
-            }
-            freq[c - 'a']--;
-            sc += score[c- 'a'];
-        }
-        if(flag == 1) {
-            // take it
-            take = sc + solve(words, freq, score, cur+1);
-        }
-        for(char c:words[cur]) {
-            freq[c - 'a']++;
-        }
-        return max(take, noTake);
-    }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
         vector<int>freq(26, 0);
         for(char c:letters) {
             freq[c - 'a']++;
         }
-        return solve(words, freq, score, 0);
+        return solve(words, score, freq, 0);
+    }
+    int solve(vector<string>&words, vector<int>&score, vector<int>&freq, int cur) {
+        if(cur >= words.size()) return 0;
+        int noTake = solve(words, score, freq, cur + 1);
+        int take = 0, flag = 0, curScore = 0;
+        string str = words[cur];
+        for(char c:str) {
+            if(freq[c - 'a'] == 0) {
+                flag = 1;
+            }
+            freq[c - 'a']--;
+            curScore += score[c - 'a'];
+        }
+        if(flag == 0) {
+            // take
+            take = curScore + solve(words, score, freq, cur+1);
+        }
+        for(char c:str) {
+            freq[c - 'a']++;
+        }
+        return max(take, noTake);
     }
 };
