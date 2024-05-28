242291983f66d3f22458d50b418b1adb3df923eb
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-28 06:43:28 +0530

    Time: 17 ms (26.06%), Space: 15.2 MB (74.30%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,31 +1,15 @@
 class Solution {
 public:
-    int lcs(string &word1, string &word2, int i, int j, vector<vector<int>>&dp) {
-        if(i == word1.size()) return word2.size() - j;
-        if(j == word2.size()) return word1.size() - i;
+    int solve(string &s1, string &s2, int i, int j, vector<vector<int>>&dp) {
+        if(i == s1.size()) return s2.size() - j;
+        if(j == s2.size()) return s1.size() - i;
         if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j]) {
-            return dp[i][j] = lcs(word1, word2, i+1, j+1, dp);
-        }
-        else {
-            return dp[i][j] = 1 + min(lcs(word1, word2, i+1, j, dp), lcs(word1, word2, i, j+1, dp));
-        }
+        if(s1[i] == s2[j]) return dp[i][j] = solve(s1, s2, i+1, j+1, dp);
+        else return dp[i][j] = 1 + min(solve(s1, s2, i+1, j, dp), solve(s1, s2, i, j+1, dp));
     }
     int minDistance(string word1, string word2) {
         int n = word1.size(), m = word2.size();
-        vector<vector<int>>dp(n + 1, vector<int>(m + 1, 0));
-        for(int i=0; i<=n; i++) dp[i][0] = i;
-        for(int j=0; j<=m; j++) dp[0][j] = j;
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=m; j++) {
-                if(word1[i-1] == word2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                else {
-                    dp[i][j] = 1 + min(dp[i-1][j], dp[i][j-1]);
-                }
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>>dp(n, vector<int>(m, -1));
+        return solve(word1, word2, 0, 0, dp);
     }
 };
