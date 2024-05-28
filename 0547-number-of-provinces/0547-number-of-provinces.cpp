511a5296e761ba4eb15f2c72b832586392043bcc
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-28 08:49:39 +0530

    Time: 19 ms (73.20%), Space: 18 MB (87.28%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,47 @@
+class UnionFind {
+public:
+    vector<int>parent, rank;
+    UnionFind(int size) {
+        parent.resize(size, -1);
+        rank.resize(size, 1);
+    }
+    int find(int u) {
+        if(parent[u] == -1) return u;
+        return parent[u] = find(parent[u]);
+    }
+    void _union(int u, int v) {
+        int parentU = find(u);
+        int parentV = find(v);
+        if(parentU == parentV) return;
+        if(rank[parentU] > rank[parentV]) {
+            rank[parentU] += rank[parentV];
+            parent[parentV] = parentU; 
+        }
+        else {
+            rank[parentV] += rank[parentU];
+            parent[parentU] = parentV;
+        }
+    }
+};
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size(), cnt = 0;
-        vector<int>adj[n];
+        int n = isConnected.size();
+        UnionFind uf(n);
         for(int i=0; i<n; i++) {
             for(int j=0; j<n; j++) {
                 if(isConnected[i][j]) {
-                    adj[i].push_back(j);
+                    int u = uf.find(i);
+                    int v = uf.find(j);
+                    uf._union(u, v);
                 }
             }
         }
-        vector<int>vis(n, 0);
+        int cnt = 0;
         for(int i=0; i<n; i++) {
-            if(vis[i] == 0) {
-                dfs(adj, i, vis);
-                cnt++;
-            }
+            if(uf.parent[i] == -1) cnt++;
+            
         }
         return cnt;
-    }
-    void dfs(vector<int>adj[], int cur, vector<int>&vis) {
-        if(vis[cur]) return;
-        vis[cur] = 1;
-        for(int child:adj[cur]) {
-            dfs(adj, child, vis);
-        }
     }
 };
