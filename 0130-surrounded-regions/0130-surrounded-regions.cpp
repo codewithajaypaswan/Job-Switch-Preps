6b16ea41aa7ce5989e9eaa9b843e4e49ac45bb25
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-29 04:45:19 +0530

    Time: 14 ms (31.63%), Space: 13.4 MB (87.13%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -2,13 +2,6 @@ class Solution {
 public:
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
-    void dfs(vector<vector<char>>&mat, int i, int j) {
-        if(i < 0 || i >= mat.size() || j < 0 || j >= mat[0].size() || mat[i][j] != 'O') return;
-        mat[i][j] = '1';
-        for(int k=0; k<4; k++) {
-            dfs(mat, i + dx[k], j + dy[k]);
-        }
-    }
     void solve(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
         for(int i=0; i<n; i++) {
@@ -24,6 +17,13 @@ public:
                 else if(board[i][j] == 'O') board[i][j] = 'X';
                 
             }
+        }
+    }
+    void dfs(vector<vector<char>>&mat, int i, int j) {
+        if(i < 0 || i >= mat.size() || j < 0 || j >= mat[0].size() || mat[i][j] != 'O') return;
+        mat[i][j] = '1';
+        for(int k=0; k<4; k++) {
+            dfs(mat, i + dx[k], j + dy[k]);
         }
     }
 };
