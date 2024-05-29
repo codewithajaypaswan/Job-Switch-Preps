cc77abeb8530987107fdce4d66b29365f4420b82
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-29 06:37:54 +0530

    Time: 11 ms (81.93%), Space: 48.2 MB (56.42%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,5 +1,5 @@
 class Solution {
-    // const mx = 1_000_000_000;
+    static final int mx = 1_000_000_000;
     public int networkDelayTime(int[][] times, int n, int k) {
         List<List<int[]>> adj = new ArrayList<>();
         for(int i=0; i<=n; i++) {
@@ -11,7 +11,7 @@ class Solution {
         PriorityQueue<int[]>pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[0]));
         int[] dist = new int[n+1];
         int[] vis = new int[n+1];
-        Arrays.fill(dist, 1_000_000_000);
+        Arrays.fill(dist, mx);
         dist[k] = 0;
         pq.offer(new int[] {0, k});
         while(!pq.isEmpty()) {
@@ -30,6 +30,6 @@ class Solution {
         for(int i=1; i<=n; i++) {
             ans = Math.max(ans, dist[i]);
         }
-        return ans == 1_000_000_000 ? -1 : ans;
+        return ans == mx ? -1 : ans;
     }
 }
