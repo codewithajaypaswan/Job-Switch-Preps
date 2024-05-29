1dcad8e1d3dcc9b1ece75ee152fcfeff5c449076
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-29 06:44:43 +0530

    Time: 971 ms (5.02%), Space: 275.2 MB (5.15%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int, int>>adj[n+1];
-        for(auto t:times) {
-            adj[t[0]].push_back({t[1], t[2]});
-        }
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
-        vector<int>vis(n+1, 0), dist(n+1, 1e9);
+        vector<int>dist(n+1, 1e9);
         dist[k] = 0;
-        pq.push({0, k});
-        while(!pq.empty()) {
-            auto it = pq.top(); pq.pop();
-            int d = it.first, u = it.second;
-            vis[u] = 1;
-            for(auto[v, wt]:adj[u]) {
-                if(vis[v] == 0 && dist[v] > dist[u] + wt) {
+        for(int i=0; i<n-1; i++) {
+            for(auto t:times) {
+                int u = t[0], v = t[1], wt = t[2];
+                if(dist[u] != 1e9 && dist[v] > dist[u] + wt) {
                     dist[v] = dist[u] + wt;
-                    pq.push({dist[v], v});
                 }
             }
         }
         int ans = 0;
-        for(int i=1; i<=n; i++) {
+        for(int i=1; i<=n; i++) 
             ans = max(ans, dist[i]);
-        }
         return ans == 1e9 ? -1 : ans;
     }
 };
