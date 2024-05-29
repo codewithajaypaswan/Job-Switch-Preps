4f60c2e77bbb109a88a96c375aff477d92146e6e
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-05-29 07:57:59 +0530

    Time: 9 ms (53.60%), Space: 41.1 MB (96.55%) - LeetHub

---
+++ 0355-design-twitter/0355-design-twitter.java
@@ -0,0 +1,59 @@
+class Twitter {
+    private Map<Integer, Set<Integer>>fo;
+    private Map<Integer, List<int[]>>tw;
+    private int timer;
+    public Twitter() {
+        fo = new HashMap<>();
+        tw = new HashMap<>();
+        timer = 0;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tw.putIfAbsent(userId, new ArrayList<>());
+        tw.get(userId).add(new int[] {timer++, tweetId});
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> ans = new ArrayList<>();
+        PriorityQueue<int[]>pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+        if(tw.containsKey(userId)) {
+            for(int[] t:tw.get(userId)) {
+                pq.offer(t);
+            }
+        }
+        if(fo.containsKey(userId)) {
+            for(int user:fo.get(userId)) {
+                if(tw.containsKey(user)) {
+                    for(int[] t:tw.get(user)) {
+                        pq.offer(t);
+                    }
+                }
+            }
+        }
+        while(!pq.isEmpty()) {
+            int[] t = pq.poll();
+            ans.add(t[1]);
+            if(ans.size() == 10) return ans;
+        }
+        return ans;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        fo.putIfAbsent(followerId, new HashSet<>());
+        fo.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        fo.putIfAbsent(followerId, new HashSet<>());
+        fo.get(followerId).remove(followeeId);
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
