4fc153eb4a83d2c97f00efd51d2dc1876104ec20
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-05 08:31:30 +0530

    Time: 3 ms (85.57%), Space: 17.2 MB (5.09%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -2,13 +2,17 @@ class Solution {
 public:
     #define ll long long
     int maxProduct(vector<int>& nums) {
-        ll mx = 1, mn = 1, ans = INT_MIN;
-        for(int i=0; i<nums.size(); i++) {
-            if(nums[i] < 0) swap(mx, mn);
-            if(mx*nums[i] >= INT_MAX || mn*nums[i] <= INT_MIN) continue;
-            mx = max((ll)nums[i], mx * nums[i]);
-            mn = min((ll)nums[i], mn * nums[i]);
-            ans = max(ans, mx);
+        int n = nums.size();
+        vector<ll>minVal(n), maxVal(n);
+        minVal[0] = maxVal[0] = nums[0];
+        for(int i=1; i<n; i++) {
+            if(minVal[i-1] * nums[i] < INT_MIN || maxVal[i-1] * nums[i] > INT_MAX) continue;
+            maxVal[i] = max({(ll) nums[i], minVal[i-1] * nums[i], maxVal[i-1] * nums[i]});
+            minVal[i] = min({(ll) nums[i], maxVal[i-1] * nums[i], minVal[i-1] * nums[i]});
+        }
+        ll ans = INT_MIN;
+        for(int i=0; i<n; i++) {
+            ans = max((ll)ans, maxVal[i]);
         }
         return ans;
     }
