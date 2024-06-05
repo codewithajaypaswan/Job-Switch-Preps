5de8570a83c30025a39758bedccc716d009079fa
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-05 08:04:26 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (63.08%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -7,7 +7,14 @@ public:
         return dp[x][y] = solve(x+1, y, m, n, dp) + solve(x, y+1, m, n, dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m+1, vector<int>(n+1, -1));
-        return solve(1, 1, m, n, dp);
+        vector<vector<int>>dp(m+1, vector<int>(n+1, 0));
+        for(int i=0; i<m; i++) dp[i][0] = 1;
+        for(int j=0; j<n; j++) dp[0][j] = 1;
+        for(int i=1; i<m; i++) {
+            for(int j=1; j<n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
