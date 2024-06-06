55680bdb14f7539fa73916922fe0b902345b99f5
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-06 07:03:53 +0530

    Time: 0 ms (100.00%), Space: 9.9 MB (88.61%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& mat) {
         int n = mat.size(), m = mat[0].size();
-        vector<vector<int>>dp(n, vector<int>(m, 0));
         for(int i=0; i<n; i++) {
-            if(mat[i][0]) break;
-            dp[i][0] = 1;
-        }
-        for(int j=0; j<m; j++) {
-            if(mat[0][j]) break;
-            dp[0][j] = 1;
-        }
-        for(int i=1; i<n; i++) {
-            for(int j=1; j<m; j++) {
-                if(mat[i][j]) dp[i][j] = 0;
-                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            int flag = 0;
+            for(int j=0; j<m; j++) {
+                int top = i == 0 ? 0 : mat[i-1][j];
+                int left = j == 0 ? 0 : mat[i][j-1];
+                if(i == 0 && j == 0 && mat[i][j] == 0) mat[i][j] = 1;
+                else mat[i][j] = mat[i][j] == 1 ? 0 : top + left;
+                
+                if(mat[i][j] > 0) flag = 1;
             }
+            if(flag == 0) return 0;
         }
-        return dp[n-1][m-1];
+        return mat[n-1][m-1];
     }
 };
