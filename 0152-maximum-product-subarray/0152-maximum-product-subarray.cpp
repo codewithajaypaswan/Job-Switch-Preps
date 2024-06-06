e52d0770429df6e0e439bf5f1c66fc133f2d0268
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-06 06:37:44 +0530

    Time: 3 ms (85.63%), Space: 16.3 MB (12.08%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,16 +3,13 @@ public:
     #define ll long long
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        vector<ll>minVal(n), maxVal(n);
-        minVal[0] = maxVal[0] = nums[0];
-        for(int i=1; i<n; i++) {
-            if(minVal[i-1] * nums[i] < INT_MIN || maxVal[i-1] * nums[i] > INT_MAX) continue;
-            maxVal[i] = max({(ll) nums[i], minVal[i-1] * nums[i], maxVal[i-1] * nums[i]});
-            minVal[i] = min({(ll) nums[i], maxVal[i-1] * nums[i], minVal[i-1] * nums[i]});
-        }
-        ll ans = INT_MIN;
-        for(int i=0; i<n; i++) {
-            ans = max((ll)ans, maxVal[i]);
+        ll mx = 1, mn = 1, ans = INT_MIN;
+        for(int x:nums) {
+            if(x < 0) swap(mx, mn);
+            if(mx*x > INT_MAX || mn * x < INT_MIN) continue;
+            mx = max((ll) x, mx * x);
+            mn = min((ll) x, mn * x);
+            ans = max(ans, mx);
         }
         return ans;
     }
