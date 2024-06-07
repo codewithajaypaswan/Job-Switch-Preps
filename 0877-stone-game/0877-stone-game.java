17e96117aa7474d65a9c595ba5e7b412c3472197
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-08 00:59:56 +0530

    Time: 8 ms (29.36%), Space: 43.9 MB (35.04%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,5 +1,15 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(piles, dp, 0, n-1) > 0;
+    }
+    int solve(int[] piles, int[][] dp, int i, int j) {
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = Math.max(piles[i] - solve(piles, dp, i+1, j), piles[j] - solve(piles, dp, i, j-1));
     }
 }
