136f45f216dc132888e3643ec4ff62da1df6c6ef
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-08 01:06:20 +0530

    Time: 11 ms (14.21%), Space: 45 MB (13.33%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,15 +1,23 @@
 class Solution {
+    int[][][] dp;
     public boolean stoneGame(int[] piles) {
         int n = piles.length;
-        int[][] dp = new int[n][n];
-        for(int i=0; i<n; i++) {
-            Arrays.fill(dp[i], -1);
+        int[][][] dp = new int[2][n][n];
+        for(int[][] arr:dp) {
+            for(int[] sub:arr) Arrays.fill(sub, -1);
         }
-        return solve(piles, dp, 0, n-1) > 0;
+        return solve(piles, dp, 0, n-1, 1) > 0;
     }
-    int solve(int[] piles, int[][] dp, int i, int j) {
+    int solve(int[] piles, int[][][] dp, int i, int j, int person) {
         if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = Math.max(piles[i] - solve(piles, dp, i+1, j), piles[j] - solve(piles, dp, i, j-1));
+        if(dp[person][i][j] != -1) return dp[person][i][j];
+        if(person == 1) {
+            // Alice will maximize the score
+            return dp[person][i][j] = Math.max(piles[i] - solve(piles, dp, i+1, j, 0), piles[j] - solve(piles, dp, i, j-1, 0));
+        }
+        else {
+            // Bob will minimize the score
+            return dp[person][i][j] = Math.min(-piles[i] - solve(piles, dp, i+1, j, 1), -piles[j] - solve(piles, dp, i, j-1, 1));
+        }
     }
 }
