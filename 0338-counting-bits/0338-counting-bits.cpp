9eb611377c7a97b152b986f04ca900dc640922a7
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-08 01:12:44 +0530

    Time: 0 ms (100.00%), Space: 9 MB (80.08%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int count(int x) {
-        int ans = 0;
-        while(x > 0) {
-            if(x&1) ans++;
-            x = x>>1;
-        }
-        return ans;
-    }
     vector<int> countBits(int n) {
-        vector<int>ans(n+1);
+        vector<int>dp(n+1, 0);
         for(int i=1; i<=n; i++) {
-            ans[i] = count(i);
+            dp[i] = dp[(i-1)&i] + 1;
         }
-        return ans;
+        return dp;
     }
 };
