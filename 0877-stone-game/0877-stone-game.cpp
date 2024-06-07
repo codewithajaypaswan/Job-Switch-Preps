3da501c5a1a09eb1aa2ee47f9aa41aa3a63dd036
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-08 00:55:45 +0530

    Time: 20 ms (16.21%), Space: 11.6 MB (35.95%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
-    int solve(vector<int>&piles, int i, int j, vector<vector<int>>&dp) {
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = max(piles[i] - solve(piles, i+1, j, dp), piles[j] - solve(piles, i, j-1, dp));
-    }
+    int dp[2][501][501];
     bool stoneGame(vector<int>& piles) {
+        memset(dp, -1, sizeof(dp));
         int n = piles.size();
-        vector<vector<int>>dp(n, vector<int>(n, -1));
-        return solve(piles, 0, n-1, dp) > 0;
+        return solve(piles, 0, n-1, 1) > 0;
+    }
+    int solve(vector<int>&piles, int i, int j, int person) {
+        if(i > j) return 0;
+        if(dp[person][i][j] != -1) return dp[person][i][j];
+        if(person == 1) {
+            // Alice should maximize the score
+            dp[person][i][j] = max(piles[i] - solve(piles, i+1, j, 0), piles[j] - solve(piles, i, j-1, 0));
+        }
+        else {
+            dp[person][i][j] = min(-piles[i] - solve(piles, i+1, j, 1), -piles[j] - solve(piles, i, j-1, 1));
+        }
+        return dp[person][i][j];
     }
 };
