6adfea904075a8dc36a30b92d32aa4fcea599193
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-10 13:45:45 +0530

    Time: 254 ms (70.58%), Space: 9.7 MB (78.90%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -7,21 +7,22 @@ public:
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(board[i][j] == word[0]) {
-                    if(dfs(board, i, j, word, 0)) return true;
+                    if(dfs(i, j, board, word, 0)) return true;
                 }
             }
         }
         return false;
     }
-    bool dfs(vector<vector<char>>&board, int x, int y, string &word, int ind) {
-        if(ind >= word.size()) return true;
-        if(x < 0 || x >= board.size() || y < 0 || y >= board[0].size() || board[x][y] != word[ind] || board[x][y] == '.') return false;
-        char ch = board[x][y];
-        board[x][y] = '.';
+    
+    bool dfs(int row, int col, vector<vector<char>>&board, string &word, int cur) {
+        if(cur >= word.size()) return true;
+        if(row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[cur] || board[row][col] == '.') return false;
+        char ch = word[cur];
+        board[row][col] = '.';
         for(int i=0; i<4; i++) {
-            if(dfs(board, x + dx[i], y + dy[i], word, ind+1)) return true;
+            if(dfs(row + dx[i], col + dy[i], board, word, cur+1)) return true;
         }
-        board[x][y] = ch;
+        board[row][col] = ch;
         return false;
     }
 };
