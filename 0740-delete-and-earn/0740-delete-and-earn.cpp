5ef0d4df33d67ec0c7b37e2a058c24a65b387e40
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-12 11:48:17 +0530

    Time: 8 ms (69.21%), Space: 14 MB (93.40%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
         int mx = *max_element(nums.begin(), nums.end());
-        vector<int>dp(mx+1, -1), freq(mx+1, 0);
+        vector<int>dp(mx+1, 0), freq(mx+1, 0);
         for(int x:nums) {
             freq[x] += x;
         }
-        return solve(nums, dp, freq, 0);
+        dp[0] = freq[0];
+        dp[1] = max(freq[0], freq[1]);
+        for(int i=2; i<=mx; i++) {
+            dp[i] = max(dp[i-2] + freq[i], dp[i-1]);
+        }
+        return dp[mx];
     }
     int solve(vector<int>&nums, vector<int>&dp, vector<int>&freq, int cur) {
         if(cur >= freq.size()) return 0;
