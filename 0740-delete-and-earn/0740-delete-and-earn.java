9f8889096d63403dfdf036fb36c1c7f924ac4cbf
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-12 11:53:02 +0530

    Time: 2 ms (99.69%), Space: 44.1 MB (85.14%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,17 +1,24 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
         int n = nums.length;
+        if(n == 1) return nums[0];
         int mx = Integer.MIN_VALUE;
         for(int x:nums) {
             if(mx < x) mx = x;
         }
         int[] dp = new int[mx+1];
         int[] freq = new int[mx+1];
-        Arrays.fill(dp, -1);
+        // Arrays.fill(dp, -1);
         for(int x:nums) {
             freq[x] += x;
         }
-        return solve(dp, freq, 0);
+        dp[0] = freq[0];
+        dp[1] = freq[1];
+        for(int i=2; i<=mx; i++) {
+            dp[i] = Math.max(freq[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[mx];
+        // return solve(dp, freq, 0);
     }
     private int solve(int[] dp, int[] freq, int cur) {
         if(cur >= freq.length) return 0;
