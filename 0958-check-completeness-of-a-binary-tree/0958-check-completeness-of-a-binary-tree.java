1ca7dc7f98bad2fe84a4f6e9b99e0d0ce4061e1c
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-13 15:13:15 +0530

    Time: 2 ms (8.22%), Space: 41.9 MB (64.28%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -16,8 +16,23 @@
 class Solution {
     long count = 0, sum = 0;
     public boolean isCompleteTree(TreeNode root) {
-        solve(root, 1);
-        return (count * (count + 1)/2) == sum;
+        // solve(root, 1);
+        // return (count * (count + 1)/2) == sum;
+        Queue<TreeNode> q = new LinkedList<>();
+        boolean flag = false;
+        q.add(root);
+        while(!q.isEmpty()) {
+            TreeNode cur = q.poll();
+            if(cur == null) {
+                flag = true;
+            }
+            else {
+                if(flag == true) return false;
+                q.add(cur.left);
+                q.add(cur.right);
+            }
+        }
+        return true;
     }
     private void solve(TreeNode root, long label) {
         if(root == null) return;
