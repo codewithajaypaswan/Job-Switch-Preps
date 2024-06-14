3d8aa2cc2aeb1b3d5d92b07bf11c6b1436f9b654
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-14 22:45:08 +0530

    Time: 83 ms (15.20%), Space: 62.6 MB (36.98%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -3,9 +3,11 @@ class Solution {
         int n = temp.length;
         int[] ans = new int[n];
         Stack<Integer>st = new Stack<>();
-        for(int i=n-1; i>=0; i--) {
-            while(!st.empty() && temp[st.peek()] <= temp[i]) st.pop();
-            if(!st.isEmpty()) ans[i] = st.peek() - i;
+        for(int i=0; i<n; i++) {
+            while(!st.isEmpty() && temp[st.peek()] < temp[i]) {
+                ans[st.peek()] = i - st.peek();
+                st.pop();
+            }
             st.push(i);
         }
         return ans;
