b3abde09cd1abfb4bf2494d49216001d8b7289b7
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-15 06:49:14 +0530

    Time: 2 ms (77.04%), Space: 7.1 MB (77.09%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     int helper(int n, int k) {
-        if(n == 1) return 0;
-        return (helper(n-1, k) + k)%n;
+        // if(n == 1) return 0;
+        // return (helper(n-1, k) + k)%n;
+        int ans = 0;
+        for(int i=1; i<=n; i++) {
+            ans = (ans + k)%i;
+        }
+        return ans;
     }
     int findTheWinner(int n, int k) {
        return helper(n, k) + 1;
