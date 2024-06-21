e3e57180e4cdb2f39b1d13fbb2a1bf5b276b35b4
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-21 10:59:41 +0530

    Time: 21 ms (76.97%), Space: 34.4 MB (16.52%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& c, vector<int>& g, int minutes) {
-        int n = c.size(), ans = 0;
+        int n = c.size(), sum = 0;
         for(int i=0; i<n; i++) {
-            if(g[i] == 0) ans += c[i];
+            if(g[i] == 0) sum += c[i];
         }
         int slidingSum = 0;
         for(int high = 0, low = 0, temp = 0; high < n; high++) {
             if(g[high] == 1) temp += c[high];
             if(high - low + 1 == minutes) {
                 slidingSum = max(slidingSum, temp);
-                // cout<<slidingSum<<" "<<temp<<endl;
                 if(g[low] == 1) temp -= c[low];
                 low++;
             }
         }
-        // cout<<ans << " "<<slidingSum<<endl;
-        return ans + slidingSum;
+        return sum + slidingSum;
     }
 };
