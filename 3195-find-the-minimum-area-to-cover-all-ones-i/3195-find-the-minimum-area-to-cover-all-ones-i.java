65ffc6513a6ddd01946e2fe23a6f5fb43c3644ee
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-24 12:13:24 +0530

    Time: 5 ms (100.00%), Space: 158.4 MB (100.00%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.java
@@ -1,20 +1,26 @@
 class Solution {
     public int minimumArea(int[][] grid) {
         int n = grid.length, m = grid[0].length;
-        List<Integer> horizontal = new ArrayList<>();
-        List<Integer> vertical = new ArrayList<>();
+        // List<Integer> horizontal = new ArrayList<>();
+        // List<Integer> vertical = new ArrayList<>();
+        int h1 = Integer.MAX_VALUE, h2 = Integer.MIN_VALUE;
+        int v1 = Integer.MAX_VALUE, v2 = Integer.MIN_VALUE;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 1) {
-                    horizontal.add(j);
-                    vertical.add(i);
+                    // horizontal.add(j);
+                    // vertical.add(i);
+                    h1 = Math.min(h1, j);
+                    h2 = Math.max(h2, j);
+                    v1 = Math.min(v1, i);
+                    v2 = Math.max(v2, i);
                 }
             }
         }
-        int h1 = minimum(horizontal);
-        int h2 = maximum(horizontal);
-        int v1 = minimum(vertical);
-        int v2 = maximum(vertical);
+        // int h1 = minimum(horizontal);
+        // int h2 = maximum(horizontal);
+        // int v1 = minimum(vertical);
+        // int v2 = maximum(vertical);
 
         return (h2 - h1 + 1) * (v2 - v1 + 1);
     }
