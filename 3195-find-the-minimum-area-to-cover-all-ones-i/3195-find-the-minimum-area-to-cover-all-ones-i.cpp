ad4c3b669c822c722a3d0be8ae54958c26a5c949
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-24 12:04:20 +0530

    Time: 244 ms (14.29%), Space: 132.3 MB (28.57%) - LeetHub

--- 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
+++ 3195-find-the-minimum-area-to-cover-all-ones-i/3195-find-the-minimum-area-to-cover-all-ones-i.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int minimumArea(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<int>horizonal, vertical;
+        // vector<int>horizonal, vertical;
+        int h1 = INT_MAX, h2 = INT_MIN;
+        int v1 = INT_MAX, v2 = INT_MIN;
         for(int i=0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == 1) {
-                    horizonal.push_back(j);
-                    vertical.push_back(i);
+                    h1 = min(h1, j);
+                    h2 = max(h2, j);
+                    v1 = min(v1, i);
+                    v2 = max(v2, i);
+                    // horizonal.push_back(j);
+                    // vertical.push_back(i);
                 }
             }
         }
-        int h1 = *min_element(horizonal.begin(), horizonal.end());
-        int h2 = *max_element(horizonal.begin(), horizonal.end());
-        int v1 = *min_element(vertical.begin(), vertical.end());
-        int v2 = *max_element(vertical.begin(), vertical.end());
+        // int h1 = *min_element(horizonal.begin(), horizonal.end());
+        // int h2 = *max_element(horizonal.begin(), horizonal.end());
+        // int v1 = *min_element(vertical.begin(), vertical.end());
+        // int v2 = *max_element(vertical.begin(), vertical.end());
         return (h2 - h1 + 1) * (v2 - v1 + 1);
     }
 };
