d3b2a55b59e6b491715f8ff4ad2f8a169503eb48
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-06-24 13:54:34 +0530

    Time: 95 ms (42.86%), Space: 76 MB (85.71%) - LeetHub

--- 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
+++ 3196-maximize-total-cost-of-alternating-subarrays/3196-maximize-total-cost-of-alternating-subarrays.cpp
@@ -12,3 +12,30 @@ public:
         return max(addResult, subResult);
     }
 };
+
+/*
+sol - 2
+class Solution {
+public:
+    #define ll long long
+    long long maximumTotalCost(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<ll>>dp(n, vector<ll>(3, INT_MAX));
+        return solve(nums, 0, dp, 0);
+    }
+    long long solve(vector<int>&nums, int cur, vector<vector<ll>>&dp, int flag) {
+        if(cur >= nums.size()) return 0;
+        if(dp[cur][flag] != INT_MAX) return dp[cur][flag];
+        ll noFlip = nums[cur] + solve(nums, cur+1, dp, 1);
+        ll flip = 0;
+        if(flag == 1) {
+            flip += (-nums[cur]) + solve(nums, cur+1, dp, 0);
+        }
+        else {
+            flip += nums[cur] + solve(nums, cur+1, dp, 1);
+        }
+        // cout<<flip<<" "<<noFlip<<endl;
+        return dp[cur][flag] = max(flip, noFlip);
+    }
+};
+*/
