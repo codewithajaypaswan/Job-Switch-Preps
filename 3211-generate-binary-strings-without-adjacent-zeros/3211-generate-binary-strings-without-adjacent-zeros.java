800cf144d6ab959b9d69bd9351e85cd962466484
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-08 15:29:41 +0530

    Time: 1 ms (100.00%), Space: 45.7 MB (100.00%) - LeetHub

--- 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
+++ 3211-generate-binary-strings-without-adjacent-zeros/3211-generate-binary-strings-without-adjacent-zeros.java
@@ -1,26 +1,30 @@
 class Solution {
-public:
-    vector<string>ans;
-    vector<string> validStrings(int n) {
-        string temp = "";
+    List<String>ans = new ArrayList<>();
+    public List<String> validStrings(int n) {
+        StringBuilder temp = new StringBuilder();
         solve(n, temp);
         return ans;
     }
-    void solve(int n, string temp) {
+    void solve(int n, StringBuilder sb) {
         if(n == 0) {
-            ans.push_back(temp);
+            ans.add(sb.toString());
             return;
         }
-        if(temp.empty()) {
-            solve(n-1, temp + '0');
-            solve(n-1, temp + '1');
+        if(sb.isEmpty()) {
+            solve(n-1, sb.append('0'));
+            sb.deleteCharAt(sb.length()-1);
+            solve(n-1, sb.append('1'));
+            sb.deleteCharAt(sb.length()-1);
         }
-        else if(temp.back() == '1') {
-            solve(n-1, temp + '1');
-            solve(n-1, temp + '0');
+        else if(sb.charAt(sb.length()-1) == '1') {
+            solve(n-1, sb.append('0'));
+            sb.deleteCharAt(sb.length()-1);
+            solve(n-1, sb.append('1'));
+            sb.deleteCharAt(sb.length()-1);
         }
         else {
-            solve(n-1, temp + '1');
+            solve(n-1, sb.append('1'));
+            sb.deleteCharAt(sb.length()-1);
         }
     }
-};
+}
