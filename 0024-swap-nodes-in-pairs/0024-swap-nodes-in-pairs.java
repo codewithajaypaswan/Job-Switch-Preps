facb9722100571d93626ae66df53cc3a059e011f
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-10 12:59:16 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (70.50%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.java
@@ -10,6 +10,26 @@
  */
 class Solution {
     public ListNode swapPairs(ListNode head) {
+        if(head == null) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy, cur = head;
+        while(cur != null && cur.next != null) {
+            ListNode nxt = cur.next;
+            cur.next = nxt.next;
+            nxt.next = cur;
+            prev.next = nxt;
+            prev = cur;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
+
+/*
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
         if(head == null || head.next == null) return head;
         ListNode temp = head.next;
         head.next = swapPairs(head.next.next);
@@ -17,3 +37,5 @@ class Solution {
         return temp;
     }
 }
+
+*/
