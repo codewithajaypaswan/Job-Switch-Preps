131756cc4a3257490ea5e2c00d3503336d1672a3
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-17 19:44:50 +0530

    Time: 8 ms (71.33%), Space: 22 MB (5.48%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,6 +11,22 @@
  */
 class Solution {
 public:
+    int solve(TreeNode* root) {
+        if(!root) return 0;
+        int lh = solve(root->left);
+        if(lh == -1) return -1;
+        int rh = solve(root->right);
+        if(rh == -1) return -1;
+        if(abs(lh - rh) > 1) return -1;
+        return max(lh, rh) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        return solve(root) == -1 ? false : true;
+    }
+};
+/*
+class Solution {
+public:
     int height(TreeNode* root) {
         if(!root) return 0;
         return 1 + max(height(root->left), height(root->right));
@@ -26,3 +42,4 @@ public:
         return solve(root);
     }
 };
+*/
