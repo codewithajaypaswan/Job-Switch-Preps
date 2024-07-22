e1a3c9d52ef868ca7b392dac91bf36378cf65133
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-22 09:32:07 +0530

    Time: 0 ms (100.00%), Space: 10.8 MB (9.28%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.cpp
@@ -11,18 +11,18 @@
  */
 class Solution {
 public:
-    TreeNode* increasingBST(TreeNode* root) {
-        if(!root) return root;
-        TreeNode* dummy = new TreeNode(0);
-        TreeNode* cur = dummy;
-        solve(root, cur);
-        return dummy->right;
-    }
-    void solve(TreeNode* root, TreeNode* &cur) {
+    TreeNode* cur = NULL;
+    void solve(TreeNode* root) {
         if(!root) return;
-        solve(root->left, cur);
+        solve(root->left);
         cur->right = new TreeNode(root->val);
         cur = cur->right;
-        solve(root->right, cur);
+        solve(root->right);
+    }
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode* dummy = new TreeNode(0);
+        cur = dummy;
+        solve(root);
+        return dummy->right;
     }
 };
