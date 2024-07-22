36b9281add5461a84a98a8287f8fef57b0f37208
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-22 09:05:13 +0530

    Time: 20 ms (84.27%), Space: 33.3 MB (27.76%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.cpp
@@ -9,6 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+class Solution {
+public:
+    TreeNode* solve(TreeNode* root1, TreeNode* root2) {
+        if(!root1) return root2;
+        if(!root2) return root1;
+        root1->val += root2->val;
+        root1->left = solve(root1->left, root2->left);
+        root1->right = solve(root1->right, root2->right);
+        return root1;
+    }
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        return solve(root1, root2);
+    }
+};
+
+/*
+
 class Solution {
 public:
     TreeNode* solve(TreeNode* root1, TreeNode* root2) {
@@ -23,3 +41,4 @@ public:
         return solve(root1, root2);
     }
 };
+*/
