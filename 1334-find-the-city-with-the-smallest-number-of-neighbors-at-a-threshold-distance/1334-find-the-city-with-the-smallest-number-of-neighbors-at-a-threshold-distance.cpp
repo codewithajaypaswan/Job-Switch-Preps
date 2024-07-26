8c4ffb4ce8a78f33e0929e780ded4acf0d4cb900
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-26 08:24:58 +0530

    Time: 49 ms (11.92%), Space: 20.7 MB (7.60%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,6 +1,45 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<pair<int, int>>adj[n];
+        for(auto e:edges) {
+            adj[e[0]].push_back({e[1], e[2]});
+            adj[e[1]].push_back({e[0], e[2]});
+        }
+        int city = -1, minCnt = INT_MAX;
+        for(int i=0; i<n; i++) {
+            priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+            vector<int>dist(n, 1e9), vis(n, 0);
+            pq.push({0, i});
+            dist[i] = 0;
+            while(!pq.empty()) {
+                auto it = pq.top(); pq.pop();
+                int u = it.second;
+                vis[u] = 1;
+                for(auto [v, wt]:adj[u]) {
+                    if(vis[v] == 0 && dist[v] > dist[u] + wt) {
+                        dist[v] = wt + dist[u];
+                        pq.push({dist[v], v});
+                    }
+                }
+            }
+            int cnt = 0;
+            for(int i=0; i<n; i++) {
+                if(dist[i] <= distanceThreshold) cnt++;
+            }
+            if(cnt <= minCnt) {
+                minCnt = cnt;
+                city = i;
+            }
+        }
+        return city;
+    }
+};
+/*
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
         vector<vector<int>>adj(n, vector<int>(n, INT_MAX));
         for(auto e:edges) {
             adj[e[0]][e[1]] = e[2];
@@ -30,3 +69,5 @@ public:
         return city;
     }
 };
+
+*/
