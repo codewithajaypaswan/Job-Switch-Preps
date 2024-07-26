0fdb50e0bc707a98e208a02f7c4b212f0b523881
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-26 08:52:51 +0530

    Time: 8 ms (83.34%), Space: 44 MB (87.34%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,5 +1,39 @@
 class Solution {
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] adj = new int[n][n];
+        for(int i=0; i<n; i++) {
+            Arrays.fill(adj[i], Integer.MAX_VALUE);
+        }
+        for(int []e:edges) {
+            adj[e[0]][e[1]] = e[2];
+            adj[e[1]][e[0]] = e[2];
+        }
+        for(int via = 0; via < n; via++) {
+            for(int i=0; i<n; i++) {
+                for(int j=0; j<n; j++) {
+                    if(adj[i][via] == Integer.MAX_VALUE || adj[via][j] == Integer.MAX_VALUE) continue;
+                    adj[i][j] = Math.min(adj[i][j], adj[i][via] + adj[via][j]);
+                }
+            }
+        }
+        int city = -1, minCnt = Integer.MAX_VALUE;
+        for(int i=0; i<n; i++) {
+            int cnt = 0;
+            for(int j=0; j<n; j++) {
+                if(i != j && adj[i][j] <= distanceThreshold) cnt++;
+            }
+            if(cnt <= minCnt) {
+                minCnt = cnt;
+                city = i;
+            }
+        }
+        return city;
+    }
+}
+/*
+
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
         List<int[]>[] adj = new ArrayList[n];
         for(int i=0; i<n; i++) {
             adj[i] = new ArrayList<>();
@@ -40,3 +74,5 @@ class Solution {
         return city;
     }
 }
+
+*/
