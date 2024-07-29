7bb7c91113fecd29b13a306b66635e216128e50e
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-29 08:56:19 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (38.22%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -6,7 +6,8 @@ public:
         // we can say G(n) = F(1, n) + F(2, n) + ... F(n, n);
         // F(3, 7)  array = [1, 2, 3(root), 4, 5, 6, 7]
         // F(3, 7) : G(2) - {1, 2} * G(4) - {4, 5, 6, 7}
-        // G(n) : F(n-1) * F(0) + F(n-2) * F(1) + F(n-3) * F(2) + .... F(0) * F(n-1)
+        // F(i, n) = G(i-1) * G(n-i)
+        // G(n) : G(n) = G(0) * G(n-1) + G(1) * G(n-2) + … + G(n-1) * G(0) 
         vector<int>dp(n+1, 0);
         dp[0] = dp[1] = 1;
         for(int i=2; i<=n; i++) {
