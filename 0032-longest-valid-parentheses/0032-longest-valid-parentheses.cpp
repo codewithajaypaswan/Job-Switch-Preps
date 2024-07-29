16522d3e0414af912371f75a7f334e22c3b2b733
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-29 15:50:10 +0530

    Time: 7 ms (24.92%), Space: 9.5 MB (10.12%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n = s.size(), ans = 0;
+        stack<int>num, ch;
+        num.push(-1);
+        for(int i=0; i<n; i++) {
+            if(s[i] == '(') {
+                num.push(i);
+                ch.push(i);
+            }
+            else if(s[i] == ')' && !ch.empty()) {
+                ch.pop(); num.pop();
+                ans = max(ans, i - num.top());
+            }
+            else {
+                num.push(i);
+            }
+        }
+        return ans;
+    }
+};
