39562770d262752a3964bd3634125ccbeb40dbf4
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-07-29 06:08:57 +0530

    Time: 79 ms (97.95%), Space: 63 MB (65.00%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.cpp
+++ 0706-design-hashmap/0706-design-hashmap.cpp
@@ -2,10 +2,10 @@ struct Node {
 public:
     int key, val;
     Node* next;
-    Node(int k, int v, Node* n) {
+    Node(int k, int v) {
         key = k;
         val = v;
-        next = n;
+        next = NULL;
     }
 };
 class MyHashMap {
@@ -19,7 +19,8 @@ public:
     void put(int key, int val) {
         remove(key);
         int h = hash(key);
-        Node* node = new Node(key, val, data[h]);
+        Node* node = new Node(key, val);
+        node->next = data[h];
         data[h] = node;
     }    
     int get(int key) {
@@ -36,12 +37,17 @@ public:
         if (node->key == key) {
             data[h] = node->next;
             delete node;
-        } else for (; node->next != NULL; node = node->next)
-            if (node->next->key == key) {
-                Node* temp = node->next;
-                node->next = temp->next;
-                delete temp;
-                return;
+        } else {
+            while(node->next != NULL) {
+                if(node->next->key == key) {
+                    Node* temp = node->next;
+                    node->next = temp->next;
+                    temp->next = NULL;
+                    delete temp;
+                    return;
+                }
+                node = node->next;
             }
+        }
     }
 };
