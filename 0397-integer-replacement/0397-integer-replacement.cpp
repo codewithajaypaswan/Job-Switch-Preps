b0016a20a2906c17f4b8b77e449851c3d6149d2f
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-08-08 07:44:18 +0530

    Time: 2 ms (61.36%), Space: 7.4 MB (57.46%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,4 +1,23 @@
 class Solution {
+public:
+    // ex 59 -> 60 -> 30 -> 15 -> 16 -> 8 -> 4 -> 2 -> 1.   59 -> 58 -> 29 -> 28 -> 14 -> 7 -> 6 -> 3 -> 2 -> 1
+    int integerReplacement(int n) {
+        if(n==INT_MAX) return 32;
+        int cnt = 0;
+        while(n>1) {
+            if(n%2==0) n = n/2;
+            else {
+                if((n+1)%4==0 && n != 3) n++;
+                else n--;
+            }
+            cnt++;
+        }
+        return cnt;
+    }
+};
+/*
+
+class Solution {
 private:
     unordered_map<int, int> visited;
 
@@ -15,3 +34,5 @@ public:
         
     }
 };
+
+*/
