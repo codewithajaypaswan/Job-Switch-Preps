1960e8daf902428c0f730c3b9b8012472eb410b8
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-08-21 11:57:36 +0530

    Time: 227 ms (63.65%), Space: 12.7 MB (68.96%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        vector<int>ans;
+        int n = arr.size();
         sort(arr.begin(), arr.end());
-        priority_queue<pair<double, pair<int, int>>, vector<pair<double, pair<int, int>>>, greater<pair<double, pair<int, int>>>>pq;
-        for(int i=0; i<arr.size(); i++) {
-            pq.push({(double)1.0 * arr[0]/arr[i], {0, i}});
+        priority_queue<pair<double, pair<int,int>>, vector<pair<double, pair<int,int>>>, greater<pair<double, pair<int,int>>>>pq;
+        for(int i=0; i<n; i++) {
+            pq.push({(double) arr[0]/arr[i], {0, i}});
         }
         while(k-- > 1 && !pq.empty()) {
             auto it = pq.top(); pq.pop();
-            double val = it.first;
             int x = it.second.first, y = it.second.second;
-            if(x+1 < arr.size()) {
-                pq.push({(double)arr[x+1]/arr[y], {x+1, y}});
+            if(x + 1 < n) {
+                pq.push({(double) arr[x+1]/arr[y], {x+1, y}});
             }
         }
-        int x = pq.top().second.first, y = pq.top().second.second;
-        return {arr[x], arr[y]};
+        if(pq.empty()) return {-1, -1};
+        auto it = pq.top();
+        return {arr[it.second.first], arr[it.second.second]};
     }
 };
