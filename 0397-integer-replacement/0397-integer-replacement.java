b7b366e07686df6b4e58a47111fd7d79f12c594b
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-08-23 13:06:41 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (59.12%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.java
+++ 0397-integer-replacement/0397-integer-replacement.java
@@ -1,17 +1,17 @@
 class Solution {
-private:
-    unordered_map<int, int> visited;
-
-public:
-    int integerReplacement(int n) {        
-        if (n == 1) { return 0; }
-        if(visited.count(n)) return visited[n];  
-        if (n%2 == 1) {
-            visited[n] = 2 + min(integerReplacement(n / 2), integerReplacement(n / 2 + 1));
-        } else {
-            visited[n] = 1 + integerReplacement(n / 2);
+    public int integerReplacement(int n) {
+        if(n == Integer.MAX_VALUE) return 32;
+        int ans = 0;
+        while(n > 1) {
+            if(n%2 == 0) {
+                n = n/2;
+            }
+            else {
+                if(n != 3 && (n+1)%4 == 0) n = n+1;
+                else n = n - 1;
+            }
+            ans++;
         }
-        return visited[n];
-        
+        return ans;
     }
-};
+}
