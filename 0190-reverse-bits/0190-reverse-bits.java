3f5bd9e75ebcb174f26eccc53b58b472e281883a
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-08-23 14:02:07 +0530

    Time: 0 ms (100.00%), Space: 41.7 MB (42.23%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -3,9 +3,9 @@ public class Solution {
     public int reverseBits(int n) {
         int ans = 0;
         for(int i=0; i<32; i++) {
-            ans = (ans<<1);
+            ans = (ans << 1);
             ans |= (n&1);
-            n = n>>1;
+            n = (n >> 1);
         }
         return ans;
     }
