ba80d0932d7c47c21eb5dedcea7a7b120600c970
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-08-27 20:36:15 +0530

    Time: 132 ms (57.81%), Space: 69.2 MB (74.41%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.cpp
@@ -6,26 +6,22 @@ public:
             adj[edges[i][0]].push_back({edges[i][1], succProb[i]});
             adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
-        vector<int>vis(n, 0);
         priority_queue<pair<double, int>>pq;
+        vector<int>vis(n, 0);
+        vector<double>prob(n, 0.0);
+        prob[start_node] = 1.0;
         pq.push({1.0, start_node});
-        vector<double>ans(n, 0.0);
-        ans[start_node] = 1.0;
         while(!pq.empty()) {
-            auto it = pq.top(); pq.pop();
-            double prob = it.first;
-            int u = it.second;
-            if(u == end_node) return prob;
+            auto[cur_prob, u] = pq.top(); pq.pop();
+            if(u == end_node) return cur_prob;
             vis[u] = 1;
-            for(auto child:adj[u]) {
-                int v = child.first;
-                double nextProb = child.second;
-                if(vis[v] == 0 && ans[v] < prob * nextProb) {
-                    ans[v] = prob * nextProb;
-                    pq.push({prob * nextProb, v});
+            for(auto[v, next_prob]:adj[u]) {
+                if(vis[v] == 0 && prob[v] < cur_prob * next_prob) {
+                    prob[v] = cur_prob * next_prob;
+                    pq.push({prob[v], v});
                 }
             }
         }
-        return ans[end_node];
+        return prob[end_node];
     }
 };
