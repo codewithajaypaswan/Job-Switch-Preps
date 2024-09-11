4ae88b965adf4ebd7f0a6d55fa8f061ca87a0eea
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-11 07:36:56 +0530

    Time: 299 ms (92.15%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,6 +1,25 @@
 # Write your MySQL query statement below
+# either of JOIN will work
 SELECT e1.name
-FROM Employee e1 RIGHT JOIN Employee e2
-ON e1.id = e2.managerId
+FROM Employee e1, Employee e2
+WHERE e1.id = e2.managerId
 GROUP BY e1.id
 HAVING COUNT(e1.id) >= 5
+
+# SELECT e1.name
+# FROM Employee e1 JOIN Employee e2
+# ON e1.id = e2.managerId
+# GROUP BY e1.id
+# HAVING COUNT(e1.id) >= 5
+
+# SELECT e1.name
+# FROM Employee e1 LEFT JOIN Employee e2
+# ON e1.id = e2.managerId
+# GROUP BY e1.id
+# HAVING COUNT(e1.id) >= 5
+
+# SELECT e1.name
+# FROM Employee e1 RIGHT JOIN Employee e2
+# ON e1.id = e2.managerId
+# GROUP BY e1.id
+# HAVING COUNT(e1.id) >= 5
