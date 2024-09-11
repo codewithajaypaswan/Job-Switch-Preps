db59ed2698f63c1857e4b2109c9f48c328874b49
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-11 07:07:12 +0530

    Time: 436 ms (55.13%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,4 +1,8 @@
 # Write your MySQL query statement below
+# SELECT w1.id 
+# FROM Weather w1, Weather w2
+# WHERE w1.temperature > w2.temperature AND TO_DAYS(w1.recordDate) - TO_DAYS(w2.recordDate) = 1;
+
 SELECT w1.id 
-FROM Weather w1, Weather w2
-WHERE w1.temperature > w2.temperature AND TO_DAYS(w1.recordDate) - TO_DAYS(w2.recordDate) = 1;
+FROM Weather w1 JOIN Weather w2
+ON w1.temperature > w2.temperature AND TO_DAYS(w1.recordDate) - TO_DAYS(w2.recordDate) = 1;
