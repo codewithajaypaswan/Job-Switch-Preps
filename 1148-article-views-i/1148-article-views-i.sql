30557ca6829b6ad6c75230e5ed3da9d1be8762f4
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-12 17:08:24 +0530

    Time: 431 ms (43.82%), Space: 0B (100.00%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -5,6 +5,10 @@
 # GROUP BY author_id
 # ORDER BY author_id;
 
+# SELECT DISTINCT v1.author_id as id from Views v1 JOIN views v2
+# ON v1.author_id = v2.viewer_id AND v1.article_id = v2.article_id
+# ORDER BY v1.author_id;
+
 SELECT DISTINCT v1.author_id as id from Views v1, views v2
 WHERE v1.author_id = v2.viewer_id AND v1.article_id = v2.article_id
 ORDER BY v1.author_id;
