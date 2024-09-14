faa0c1bf461e80cbe22d18222e825a9c57dc84cb
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-14 08:06:56 +0530

    Time: 18 ms (92.36%), Space: 33.6 MB (27.05%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,41 +12,23 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(!root) return root;
-        if(root->val == key) return del(root);
-        TreeNode* cur = root;
-        while(cur) {
-            if(cur->val < key) {
-                if(cur->right && cur->right->val == key) {
-                    cur->right = del(cur->right);
-                    return root;
-                }
-                cur = cur->right;
-            }
-            else {
-                if(cur->left && cur->left->val == key) {
-                    cur->left = del(cur->left);
-                    return root;
-                }
-                cur = cur->left;
-            }
+        if (!root) return nullptr;
+        if (key < root->val) {
+            root->left = deleteNode(root->left, key);
+        } else if (key > root->val) {
+            root->right = deleteNode(root->right, key);
+        } else {
+            if (!root->left) return root->right;
+            if (!root->right) return root->left;
+            TreeNode* leftMost = findLeftMost(root->right);
+            root->val = leftMost->val;
+            root->right = deleteNode(root->right, leftMost->val);
         }
         return root;
     }
     
     TreeNode* findLeftMost(TreeNode* root) {
-        while(root->left != NULL) {
-            root = root->left;
-        } 
+        while (root && root->left) root = root->left;
         return root;
-    }
-    TreeNode* del(TreeNode* root) {
-        if(!root->left && !root->right) return NULL;
-        if(!root->left) return root->right;
-        if(!root->right) return root->left;
-        TreeNode* p1 = root->left;
-        TreeNode* leftMost = findLeftMost(root->right);
-        leftMost->left = p1;
-        return root->right;
     }
 };
