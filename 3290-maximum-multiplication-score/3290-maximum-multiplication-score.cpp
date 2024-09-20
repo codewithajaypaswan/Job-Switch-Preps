11ad5160f1a7cc2a63929227f50be57dbfcf6152
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-20 16:24:30 +0530

    Time: 179 ms (61.10%), Space: 194.7 MB (23.04%) - LeetHub

--- 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
+++ 3290-maximum-multiplication-score/3290-maximum-multiplication-score.cpp
@@ -1,5 +1,28 @@
 class Solution {
 public:
+    long long mn = -1e11;
+    long long maxScore(vector<int>& a, vector<int>& b) {
+        int n = b.size();
+        vector<vector<long long>>dp(5, vector<long long>(n+1, mn));
+        for(int i=0; i<=n; i++) {
+            dp[0][i] = 0;
+        }
+        for(int i=1; i<=4; i++) {
+            for(int j=1; j<=n; j++) {
+                long long take = dp[i-1][j-1] + (long long)a[i-1]*(long long)b[j-1];
+                long long noTake = dp[i][j-1];
+                dp[i][j] = max(take, noTake);
+            }
+        }
+        return dp[4][n];
+    }
+};
+
+/*
+
+
+class Solution {
+public:
     long long mn = -1e15;
     long long solve(vector<int>&a, vector<int>&b, int i, int j, vector<vector<long long>>&dp) {
         if(a.size() == i) return 0;
@@ -14,3 +37,5 @@ public:
         return solve(a, b, 0, 0, dp);
     }
 };
+
+*/
