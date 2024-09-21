ccaa1fc4714917186285bd318d419facb5fcc559
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-22 02:37:13 +0530

    Time: 10 ms (67.66%), Space: 12.8 MB (84.62%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -2,7 +2,21 @@ class Solution {
 public:
     vector<int>ans;
     vector<int> lexicalOrder(int n) {
-        solve(n, 1);
+        // solve(n, 1);
+        int cur = 1;
+        for(int i=1; i<=n; i++) {
+            ans.push_back(cur);
+            if(cur*10 <= n) {
+                cur = cur * 10;
+            }
+            else if(cur + 1 <= n && cur%10 != 9) {
+                cur = cur + 1;
+            }
+            else {
+                while((cur/10)%10 == 9) cur = cur / 10;
+                cur = cur/10 + 1;
+            }
+        }
         return ans;
     }
     void solve(int n, int cur) {
