4483672d07c287d796beb5eb49279f53ca79a9ca
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-23 13:15:57 +0530

    Time: 234 ms (59.49%), Space: 45.2 MB (67.47%) - LeetHub

--- 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.java
@@ -1,17 +1,17 @@
 class Solution {
     public int[] kthSmallestPrimeFraction(int[] arr, int k) {
-        PriorityQueue<Pair>pq = new PriorityQueue();
-        int n = arr.length;
-        for(int i=0; i<n; i++) {
-            pq.add(new Pair((double) arr[0]/arr[i], 0, i));
+        PriorityQueue<Pair>pq = new PriorityQueue<Pair>();
+        for(int i=0; i<arr.length; i++) {
+            pq.add(new Pair((double)arr[0]/arr[i], 0, i));
         }
-        while(k-- > 1 && !pq.isEmpty()) {
-            Pair it = pq.poll();
-            if(it.x+1 < n) {
-                pq.add(new Pair((double) arr[it.x+1]/arr[it.y], it.x+1, it.y));
+        while(k-- > 1) {
+            Pair cur = pq.poll();
+            if(cur.x + 1 < arr.length) {
+                pq.add(new Pair((double)arr[cur.x+1]/arr[cur.y], cur.x+1, cur.y));
             }
         }
-        return new int[] {arr[pq.peek().x], arr[pq.peek().y]};
+        Pair cur = pq.poll();
+        return new int[] {arr[cur.x], arr[cur.y]};
     }
     static class Pair implements Comparable<Pair> {
         double value;
