17cce84a18b70b5426cd8f5db3105e0e3729ae24
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-23 13:37:58 +0530

    Time: 43 ms (20.74%), Space: 53.9 MB (36.40%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,34 @@
+class Solution {
+    public double distance(int[] p1) {
+        return Math.sqrt((p1[0] * p1[0]) + (p1[1] * p1[1]));
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Pair>pq = new PriorityQueue<>(Collections.reverseOrder());
+        int n = points.length;
+        for(int i=0; i<n; i++) {
+            pq.add(new Pair(distance(points[i]), points[i][0], points[i][1]));
+            if(pq.size() > k) pq.poll();
+        }
+        int size = pq.size();
+        int[][] ans = new int[size][2];
+        for(int i=0; i<size; i++) {
+            Pair cur = pq.poll();
+            ans[i][0] = cur.x;
+            ans[i][1] = cur.y;
+        }
+        return ans;
+    }
+    public class Pair implements Comparable<Pair> {
+        double value;
+        int x, y;
+        Pair(double value, int x, int y) {
+            this.value = value;
+            this.x = x;
+            this.y = y;
+        }
+        
+        public int compareTo(Pair other) {
+            return Double.compare(this.value, other.value);
+        }
+    }
+}
