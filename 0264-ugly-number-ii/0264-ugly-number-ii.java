bd7a7eb0ca771cc5dd7467c24b21261421bb31d2
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-26 08:38:03 +0530

    Time: 47 ms (41.67%), Space: 44.9 MB (7.23%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        PriorityQueue<Long> pq = new PriorityQueue<>();
+        HashSet<Long> seen = new HashSet<>();
+        
+        pq.add(1L);
+        seen.add(1L);
+        
+        long cur = 1;
+        for (int i = 1; i < n; i++) {
+            cur = pq.poll();
+            if (seen.add(cur * 2)) pq.add(cur * 2);
+            if (seen.add(cur * 3)) pq.add(cur * 3);
+            if (seen.add(cur * 5)) pq.add(cur * 5);
+        }
+        
+        return (int) pq.peek().longValue();
+    }
+}
+
+/*
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int p2 = 0, p3 = 0, p5 = 0;
+        for(int i=1; i<n; i++) {
+            int mn = Math.min(dp[p2] * 2, Math.min(dp[p3] * 3, dp[p5] * 5));
+            if(mn == dp[p2] * 2) {
+                p2++;
+            }
+            if(mn == dp[p3] * 3) {
+                p3++;
+            }
+            if(mn == dp[p5] * 5) {
+                p5++;
+            }
+            dp[i] = mn;
+        }
+        return dp[n-1];
+    }
+}
+*/
