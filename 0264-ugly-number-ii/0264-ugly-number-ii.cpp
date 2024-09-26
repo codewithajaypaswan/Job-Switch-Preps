9808cf75e337e28ca1d1a702574d23021e3a6e16
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-26 08:01:47 +0530

    Time: 53 ms (36.84%), Space: 14.1 MB (39.93%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
+        priority_queue<long, vector<long>, greater<long>>pq;
+        pq.push(1L);
+        for(int i=1; i<n; i++) {
+            auto it = pq.top(); pq.pop();
+            while(!pq.empty() && it == pq.top()) {
+                pq.pop();
+            }
+            pq.push(it * 2);
+            pq.push(it * 3);
+            pq.push(it * 5);
+        }
+        return pq.top();
+    }
+};
+
+/*
+class Solution {
+public:
+    int nthUglyNumber(int n) {
         vector<int>dp(n, 0);
         dp[0] = 1;
         int p2 = 0, p3 = 0, p5 = 0;
@@ -20,3 +39,4 @@ public:
         return dp[n-1];
     }
 };
+*/
