3f7402190139bb51472ec9b87c3495bc6cce3d87
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-26 06:49:02 +0530

    Time: 7 ms (86.37%), Space: 57.5 MB (32.52%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,15 +1,17 @@
 class Solution {
     public int maximalSquare(char[][] mat) {
-        int n = mat.length, m = mat[0].length, len = 0;
+        int n = mat.length, m = mat[0].length;
         int[][] dp = new int[n+1][m+1];
+        int len = 0;
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
                 if(mat[i-1][j-1] == '1') {
-                    dp[i][j] = Math.min(Math.min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
-                    len = Math.max(len, dp[i][j]);
+                    int mn = Math.min(dp[i-1][j-1], Math.min(dp[i-1][j], dp[i][j-1])) + 1;
+                    dp[i][j] = mn;
+                    len = Math.max(len, mn);
                 }
             }
         }
-        return len*len;
+        return len * len;
     }
 }
