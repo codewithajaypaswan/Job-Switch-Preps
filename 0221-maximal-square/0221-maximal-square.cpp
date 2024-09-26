8167ff61e2c46568c93e7ec3daa7c47a61464c86
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-09-26 06:46:06 +0530

    Time: 63 ms (51.50%), Space: 24.8 MB (37.87%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,15 +3,16 @@ public:
     int maximalSquare(vector<vector<char>>& mat) {
         int n = mat.size(), m = mat[0].size();
         vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
-        int len = 0;
+        int ans = 0;
         for(int i=1; i<=n; i++) {
             for(int j=1; j<=m; j++) {
                 if(mat[i-1][j-1] == '1') {
-                    dp[i][j] = min({dp[i-1][j-1], dp[i-1][j], dp[i][j-1]}) + 1;
-                    len = max(len, dp[i][j]);
+                    int mn = min({dp[i-1][j-1], dp[i][j-1], dp[i-1][j]}) + 1;
+                    dp[i][j] = mn;
+                    ans = max(ans, mn);
                 }
             }
         }
-        return len * len;
+        return ans * ans;
     }
 };
