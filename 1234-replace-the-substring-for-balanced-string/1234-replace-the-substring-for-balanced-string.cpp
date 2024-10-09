c652c926de6c02c79cb40a0c2406583ecdc6ab60
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-10-09 17:01:03 +0530

    Time: 34 ms (67.99%), Space: 10.6 MB (23.78%) - LeetHub

--- 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
+++ 1234-replace-the-substring-for-balanced-string/1234-replace-the-substring-for-balanced-string.cpp
@@ -16,3 +16,15 @@ public:
         return ans;
     }
 };
+
+/*
+
+E.g s = QWQWERQW
+n = 8, K = n/4 = 2
+After we calculate the frequencies in the first for loop, it would be {Q=3, W=3, E=1, R=1}
+Now comes the part where we want to know the count outside the window. The idea is that if the count of each character outside the window is less than equal to K, then we can replace the elements inside the window with the missing characters (it does not matter what characters are inside the window), but if the count of any of the character outside the window is more than K, then no matter what you put inside the window, it will still remain unbalanced.
+
+e.g. When j = 1 [Q] - count outside window {Q=2, W=3, E=1, R=1} (Not possible, W = 3 > K)
+j = 2 [Q,W] - count outside window {Q=2, W=2, E=1, R=1} (Our answer as count of all chars outside window <= K)
+
+*/
