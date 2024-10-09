5769e732a1aef25121015ca17a860baa71a5f4df
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-10-10 05:01:49 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (99.23%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -3,10 +3,8 @@ class Solution {
         int leftPar = 0, rightPar = 0;
         for(char c:s.toCharArray()) {
             if(c == '(') leftPar++;
-            else if(c == ')') {
-                if(leftPar > 0) leftPar--;
-                else rightPar++;
-            }
+            else if(leftPar > 0) leftPar--;
+            else rightPar++;
         }
         return leftPar + rightPar;
     }
