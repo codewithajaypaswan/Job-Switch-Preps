ec1c939a1da5ec83984b3dd105ded6926926cd16
AJAY PASWAN <60291173+codewithajaypaswan@users.noreply.github.com>
2024-12-04 16:02:32 +0530

    Time: 290 ms (69.52%), Space: 191.4 MB (31.73%) - LeetHub

--- 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.cpp
+++ 3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.cpp
@@ -9,6 +9,27 @@ public:
         }
         int ans = INT_MIN;
         for(int x:nums) {
+            int outlier = sum - x - x;
+            if(mp[outlier] > (outlier == x ? 1 : 0)) {
+                ans = max(ans, outlier);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+public:
+    int getLargestOutlier(vector<int>& nums) {
+        unordered_map<int, int>mp;
+        long sum = 0;
+        for(int x:nums) {
+            sum += x;
+            mp[x]++;
+        }
+        int ans = INT_MIN;
+        for(int x:nums) {
             mp[x]--;
             // x is the outliner
             long req = sum - x;
@@ -26,3 +47,4 @@ public:
         return ans;
     }
 };
+*/
